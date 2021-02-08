3e4f8fd6a797c883f4dccf029bf542a1f02210d4
Jon Hill <jondhill1984@gmail.com>
2021-02-08 16:18:32 +0200

    add more enzyme tests

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
-    <div className="App">
-      <h1>Learn React Testing</h1>
-    </div>
+    <>
+      <div data-test="componentApp">
+        <h1 data-test="counterDisplay">
+          The counter is currently <span data-test="count">{count}</span>
+        </h1>
+        <button data-test="incrementButton" onClick={() => setCount(count + 1)}>
+          Increment Counter
+        </button>
+        <button data-test="decrementButton" onClick={() => setCount(count - 1)}>
+          Decrement counter
+        </button>
+      </div>
+    </>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -6,6 +6,61 @@ import App from "./App";
 
 Enzyme.configure({ adapter: new Adapter() });
 
+/**
+ * Factory function to create a ShallowWrapper for the App component
+ * @function setup
+ * @returns {ShallowWrapper}
+ */
+const setup = () => shallow(<App />);
+
+const findByTestAttribute = (wrapper, val) =>
+  wrapper.find(`[data-test='${val}']`);
+
 test("renders learn react link", () => {
-  const wrapper = shallow(<App />);
+  const wrapper = setup();
+  const appComponent = findByTestAttribute(wrapper, "componentApp");
+  expect(appComponent.length).toBe(1);
+});
+
+test("renders button", () => {
+  const wrapper = setup();
+  const button = findByTestAttribute(wrapper, "incrementButton");
+  expect(button.length).toBe(1);
+});
+
+test("renders counter display", () => {
+  const wrapper = setup();
+  const counter = findByTestAttribute(wrapper, "counterDisplay");
+  expect(counter.length).toBe(1);
+});
+test("counter starts at 0", () => {
+  const wrapper = setup();
+  const count = findByTestAttribute(wrapper, "count").text();
+  expect(count).toBe("0");
+});
+test("clicking on button increments counter display", () => {
+  // find the button
+  const wrapper = setup();
+  const button = findByTestAttribute(wrapper, "incrementButton");
+
+  //click the button
+  button.simulate("click");
+
+  // find the display and test that the number has been incremented
+  const count = findByTestAttribute(wrapper, "count").text();
+  expect(count).toBe("1");
+});
+
+test("renders decrement button", () => {
+  const wrapper = setup();
+  const decrementButton = findByTestAttribute(wrapper, "decrementButton");
+  expect(decrementButton.length).toBe(1);
+});
+
+test("clicking on the decrement button decreasements the display", () => {
+  const wrapper = setup();
+  const decrementButton = findByTestAttribute(wrapper, "decrementButton");
+  decrementButton.simulate("click");
+  const count = findByTestAttribute(wrapper, "count").text();
+  expect(count).toBe("-1");
 });
