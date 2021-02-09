41e388b9ae8815ad790d1b61409f39372a47352a
Jon Hill <jondhill1984@gmail.com>
2021-02-09 15:55:32 +0200

    Extract enzyme config into set up file

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -1,11 +1,8 @@
 import React from "react";
-import Enzyme, { shallow } from "enzyme";
-import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+import { shallow } from "enzyme";
 
 import Congrats from "./Congrats";
 import { findByTestAttribute, checkProps } from "../test/testUtils";
-
-Enzyme.configure({ adapter: new Adapter() });
 
 const defaultProps = { success: false };
 
