da324e9505afbcf29b188e9dafe82eecbfd7d4a0
Jon Hill <jondhill1984@gmail.com>
2021-02-10 16:44:02 +0200

    Add redux basic set up

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import GuessedWords from "./GuessedWords";
 import Congrats from "./Congrats";
 
-export default function App() {
-  return (
-    <>
-      <div className="container">
-        <h1>Jotto</h1>
-        <Congrats success={true} />
-        <GuessedWords
-          guessedWords={[{ guessedWord: "train", letterMatchCount: 3 }]}
-        />
-      </div>
-    </>
-  );
+class App extends Component {
+  render() {
+    return (
+      <>
+        <div className="container">
+          <h1>Jotto</h1>
+          <Congrats success={true} />
+          <GuessedWords
+            guessedWords={[{ guessedWord: "train", letterMatchCount: 3 }]}
+          />
+        </div>
+      </>
+    );
+  }
 }
+
+export default App;
---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+import succces from "./successReducer";
+
+export default combineReducers({
+  succces,
+});
