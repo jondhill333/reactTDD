97e47c4f626f99c10cfebc25f6be2792c8dbaaf5
Jon Hill <jondhill1984@gmail.com>
2021-02-17 11:17:04 +0200

    Set up guessed words test

---
+++ src/integration.test.js
@@ -0,0 +1,16 @@
+import { storeFactory } from "../test/testUtils";
+import { guessWord } from "./actions";
+
+describe("guessWord action dispatcher", () => {
+  describe("no guessed words", () => {
+    test("updates state correctly for unsucessful guess", () => {});
+
+    test("updates state correctly for sucessful guess", () => {});
+  });
+
+  describe("some guessed words", () => {
+    test("updates state correctly for unsucessful guess", () => {});
+
+    test("updates state correctly for sucessful guess", () => {});
+  });
+});
