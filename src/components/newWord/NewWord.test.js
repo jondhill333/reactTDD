a136c074375bb634e473061e45763744ffba32d8
Jon Hill <jondhill1984@gmail.com>
2021-02-23 13:37:22 +0200

    Add basic first tests

--- src/components/newWord/NewWord.test.js
+++ src/components/newWord/NewWord.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { shallow } from "enzyme";
 import { findByTestAttribute } from "../../../test/testUtils";
-import NewWord from "./newWord";
+import NewWord from "./NewWord";
 
 const defaultProps = { display: false };
 
